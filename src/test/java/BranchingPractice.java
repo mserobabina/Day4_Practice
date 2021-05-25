c247834e9b019430b7c36a236897ffadd6dd437f
Maria Serobabina <mariaserobabina@gmail.com>
2021-05-25 19:24:49 -0400

    Second commit

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,10 @@
 public class BranchingPractice {
     public static void main(String[] args) {
+        //create new project
+        //enable VCS
+        //added .gitignore
+        //made initial commit
+        //share project on GitHub
 
     }
 }
